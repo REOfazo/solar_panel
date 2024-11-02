b28813143a68f9fa9af73f77c00744500a97f376
REOfazo <94753088+REOfazo@users.noreply.github.com>
2024-11-02 07:43:59 +0500

    Update scripts.js

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,5 +1,1 @@
-document.querySelectorAll('.order-btn').forEach(button => {
-    button.addEventListener('click', () => {
-        alert("Buyurtma muvaffaqiyatli yuborildi! Biz siz bilan bog'lanamiz.");
-    });
-});
+
